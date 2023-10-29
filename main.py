d98431d98adf10f5c37db039ade740e5485366e1
Drake Fish <drake@8thlight.com>
2023-10-29 14:13:46 -0500

    adding summary memory instead

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from dotenv import load_dotenv
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory
 load_dotenv()
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(
+memory = ConversationSummaryMemory(
   memory_key="messages", 
   return_messages=True,
-  chat_memory=FileChatMessageHistory("messages.json")
+  llm=chat
 )
 
 prompt = ChatPromptTemplate(
-  input_variables = ["content", "messages "],
+  input_variables = ["content", "messages"],
   messages = [
     MessagesPlaceholder(variable_name="messages"),
     HumanMessagePromptTemplate.from_template("{content}")
