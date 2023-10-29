ea156a6b1cff966af42e4c5d3bf095f386d6dbf7
Drake Fish <drake@8thlight.com>
2023-10-29 13:33:35 -0500

    creating basic chat model

---
+++ main.py
@@ -0,0 +1,26 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+prompt = ChatPromptTemplate(
+  input_variables = ["content"],
+  messages = [
+    HumanMessagePromptTemplate.from_template("{content}")
+  ]
+)
+
+chain = LLMChain(
+  llm=chat,
+  prompt=prompt
+)
+
+while True:
+  content = input(">>")
+
+  result = chain({"content": content})
+  
+  print(result["text"])
