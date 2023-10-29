ef3e4dc43b4b4c0b1977afec17a5a70fb04a6835
Drake Fish <drake@8thlight.com>
2023-10-29 13:52:39 -0500

    adding memory

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from dotenv import load_dotenv
-
+from langchain.memory import ConversationBufferMemory
 load_dotenv()
 
 chat = ChatOpenAI()
+
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-  input_variables = ["content"],
+  input_variables = ["content", "messages "],
   messages = [
+    MessagesPlaceholder(variable_name="messages"),
     HumanMessagePromptTemplate.from_template("{content}")
   ]
 )
 
 chain = LLMChain(
   llm=chat,
-  prompt=prompt
+  prompt=prompt,
+  memory=memory
 )
 
 while True:
